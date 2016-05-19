4daa2ee596d90fb78c7b61539f778d5ffa3368f2
Shubham Bansal <shubhambansal643@gmail.com>
2016-05-19 18:37:31 +0530

    edited json parser

--- packageparser.py
+++ packageparser.py
@@ -3,4 +3,26 @@ from pprint import pprint
 
 with open('./package.json') as json_data:
     d = json.load(json_data)
-    print d["dependencies"]
+    try:
+    	s = d["dependencies"]
+    	if s:
+    		print "Dependencies"
+    		print s
+    except:
+    	pass
+    
+    try:
+    	s = d["devDependencies"]
+    	if s:
+    		print "devDependencies"
+    		print s
+    except:
+    	pass
+
+    try:
+    	s = d["peerDependencies"]
+    	if s:
+    		print "peerDependencies"
+    		print s
+    except:
+    	pass
