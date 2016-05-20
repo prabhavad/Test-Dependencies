06d468c99a989d1f7af79b9f05cc35d3c5982a10
Prabhav Adhikari <prabhavad@gmail.com>
2016-05-20 10:02:00 +0530

    update parser

--- Parser.py
+++ Parser.py
@@ -5,13 +5,10 @@ from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 
-directory = raw_input()
-if(directory[len(directory)-1]=='/'):
-	directory = directory
-else:
-	directory = directory + '/'
+directory = ''
 
-Files = ["json","txt","xml","gradle","lock"]
+
+Files = ["package.json","requirements.txt","pom.xml","build.gradle","Gemfile.lock"]
 Op = open("Dependencies.txt","w")
 
 def GemParser(file):
@@ -80,33 +77,45 @@ def JSONParser(file):
 	    except:
 	    	pass
 
-def Process(file,ext):
-	Op.write(file+'\n')
+def Process(file,direct):
+	directory = direct+'/'
+	fileName = file.split('.')
+	ext=''
+	if(len(fileName)>1):
+		ext = fileName[1]
+	else:
+		ext = '' 
+
+	Op.write(directory+file+'\n')
 	if(ext == 'json'):
-		Op.write("JSON Dependencies\n")
+		#Op.write("JSON Dependencies\n")
 		JSONParser(file)
 	elif(ext == 'txt'):
-		Op.write("Python Dependencies(.txt)\n")
+		#Op.write("Python Dependencies(.txt)\n")
 		f = open(directory+file,'r')
 		Op.write(f.read())
 		
 	elif(ext == 'gradle'):
-		Op.write("build.gradle Dependencies\n")
+		#Op.write("build.gradle Dependencies\n")
 		GradleParser(file)
 	elif(ext == 'xml'):
-		Op.write("pom.xml Dependencies\n")
+		#Op.write("pom.xml Dependencies\n")
 		PomParser(file)
 	elif(ext == 'lock'):
-		Op.write("lock Dependencies\n")
+		#Op.write("lock Dependencies\n")
 		GemParser(file)
 	Op.write ("\n-----------------------------------\n")
 
+def Scan():
+	rootDir = '.'
+	for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
+	    #print('Found directory: %s' % dirName)
+	    for fName in fileList:
+			if(fName in Files):
+					print dirName+'/'+fName
+					Process(fName,dirName)
 
 
-for files in os.listdir(directory):
-	filename = files.split('.')
-	if(len(filename)==2 and filename[0]!=""):
-		if(filename[1] in Files):
-			#print filename[1]
-			Process(files,filename[1])
+Scan()
+
 
